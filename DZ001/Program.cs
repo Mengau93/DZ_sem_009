191abbe3345973316c7b3ff9257f5d1fce1ccda4
Romka <android91@mail.ru>
2022-08-02 22:37:38 +0300

    add DZ 001 add DZ 002

---
+++ DZ001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+
+Console.Clear();
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int N, int count)
+{
+    if (count > N) return;
+    PrintNumber(N, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
